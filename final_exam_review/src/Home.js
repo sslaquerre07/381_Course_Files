492dbb6a143c7ff326ac7c13230c7e7390dc9283
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2024-04-12 08:54:38 -0600

    Some basic review exercises complete

--- final_exam_review/src/Home.js
+++ final_exam_review/src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
+import './Home.css';
 
 function Home(){
     const navigate = useNavigate();
@@ -12,6 +13,20 @@ function Home(){
     <div>
         <h1>Hello World!</h1>
         <button onClick={goToLogin}>Go to Login</button>
+        <div className='checkBoxes'>
+            <div className='optionContainer'>
+                <label htmlFor='option1'>Option1</label>
+                <input id='option1' type='checkbox'></input>
+            </div>
+            <div className='optionContainer'>
+                <label htmlFor='option2'>Option2</label>
+                <input id='option2' type='checkbox'></input>
+            </div>
+            <div className='optionContainer'>
+                <label htmlFor='option3'>Option3</label>
+                <input id='option3' type='checkbox'></input>
+            </div>
+        </div>
     </div>
     );
 }
--- final_exam_review/src/Login.js
+++ final_exam_review/src/Login.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css';
 
 function Login(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordC, setPasswordC] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (event) => {
+        if(password === passwordC && password !== ""){
+            setMessage("Success");
+        }
+        else if(password === ""){
+            setMessage("Password must be at least one character");
+        }
+        else{
+            setMessage("Passwords are different");
+        }
+        event.preventDefault();
+    }
+
     return(
         <div className='loginField'>
-            <label htmlFor='username'>Username:</label>
-            <input id='username' type='textarea' placeholder='username'></input>
-            <br/>
-            <label htmlFor='password'>Password:</label>
-            <input id='password' type='password'></input>
-            <label htmlFor='passwordC'>Confirm Password:</label>
-            <input id='passwordC' type='password'></input>
+            <form onSubmit={handleSubmit}>
+                <p>{message}</p>
+                <label htmlFor='username'>Username:</label>
+                <input id='username' type='textarea' placeholder='username' onChange={(e) => setUsername(e.target.value)}></input>
+                <br/>
+                <label htmlFor='password'>Password:</label>
+                <input id='password' type='password' onChange={(e) => setPassword(e.target.value)}></input>
+                <br />
+                <label htmlFor='passwordC'>Confirm Password:</label>
+                <input id='passwordC' type='password' onChange={(e) => setPasswordC(e.target.value)}></input>
+                <br />
+                <button type='submit'>Submit</button>
+            </form>
+            <a href='/'>Home</a>
         </div>
     );
 }
